f28767eef9383285177a78f9c7bbcbc3309124de
Utkarsh Sharma <utkarshstudent1@gmail.com>
2020-12-05 13:21:25 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/utkarshsharma/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.utkarshsharma.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/loading.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: CircularProgressIndicator(
+        backgroundColor: Colors.orange,
+      ),),
+    );
+  }
+}
