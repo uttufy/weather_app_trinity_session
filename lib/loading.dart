9bc7f33d26665fe3cfa268b98e82251c54e69e30
Utkarsh Sharma <utkarshstudent1@gmail.com>
2020-12-05 13:39:27 +0530

    Some Minor Changes

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,8 +7,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  LocationHelper locationData ;
+
+  Future<void> getLocation() async {
+    locationData = LocationHelper();
+    await locationData.getCurrentLocation();
+    if (locationData.latitude == null || locationData.longitude == null) {
+      print("Failed to get GPS coord.");
+    }
+  }
+
+
+  void getWeatherData(){
+    // 1. Get GPS coordinates
+    // 2. API call
+    // 3. Json response Parsing
+    // 4. Parsed to be transfered to Main Screen
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getWeatherData();
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
           child: CircularProgressIndicator(
